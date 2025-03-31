be9361a44bce50a8ffdaaf9aa9ac4d11e3125150
RooXChicken <rooxchicken@gmail.com>
2025-03-30 23:43:07 -0400

    Fix background image and begin adding questions with scoring points. Most questions and points are in the JSON, but I'm still waiting on #12 and points for #10 and #11

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const nextButton = document.getElementById("nextButton");
 const progressLabel = document.getElementById("progressLabel");
 const questionLabel = document.getElementById("questionLabel");
 
-const data = JSON.parse("{ \"questionCount\": 3, \"question0\": [\"This\", \"is\", \"a\", \"test\", \"What is this?\"], \"question1\": [\"Red\", \"Blue\", \"Green\", \"Yellow\", \"What is your favorite color?\"], \"question2\": [\"1\", \"2\", \"3\", \"4\", \"Pick a number\"]}");
+const data = JSON.parse("{\"questionCount\": 15,\"question0\": [3, \"When tackling a task, do you prefer to:\", {\"answer\": \"Work as a team\"}, {\"answer\": \"Work by yourself\"}, {\"answer\": \"It depends\"}],\"question1\": [3, \"When working within a group, do you prefer to:\", {\"answer\": \"Take charge\"}, {\"answer\": \"Let others make the decisions\"}, {\"answer\": \"It depends\"}],\"question2\": [3, \"When faced with a problem, do you:\", {\"answer\": \"Trust your instincts\"}, {\"answer\": \"Use logic and facts to determine the best outcome\"}, {\"answer\": \"Look to others for guidance\"}],\"question3\": [3, \"Do you tend to embrace change, or prefer stability:\", {\"answer\": \"Change\"}, {\"answer\": \"Stability\"}, {\"answer\": \"It depends\"}],\"question4\": [4, \"If your friend is struggling with a problem, do you:\", {\"answer\": \"Let them figure it out\"}, {\"answer\": \"Feel worried for them\"}, {\"answer\": \"Give them solutions to try and help\"}, {\"answer\": \"Offer to help in whatever way they need\"}],\"question5\": [3, \"If you had a day off, would you:\", {\"answer\": \"Do nothing\"}, {\"answer\": \"Use the time to peruse your other goals / hobbies\"}, {\"answer\": \"It depends\"}],\"question6\": [3, \"Do you prefer to express any feelings or concerns to others, or keep your thoughts to yourself?\", {\"answer\": \"Express your feelings\"}, {\"answer\": \"Keep your feelings to yourself\"}, {\"answer\": \"It depends\"}],\"question7\": [3, \"When problem solving, do you tend to make up your own solutions, or research the best course of action?\", {\"answer\": \"Come up with your own solutions\"}, {\"answer\": \"Research\"}, {\"answer\": \"A bit of both\"}],\"question8\": [3, \"If you hear someone arguing about something, do you give them privacy, or listen closer?\", {\"answer\": \"Give them privacy\"}, {\"answer\": \"Listen closer\"}, {\"answer\": \"It depends\"}],\"question9\": [3, \"When problem solving, can you come up with solutions quickly or does it take you a bit of time?\", {\"answer\": \"Can come up with solutions quickly\"}, {\"answer\": \"It takes you a bit of time\"}, {\"answer\": \"It depends\"}],\"question10\": [4, \"If someone asks for your opinion, do you:\", {\"answer\": \"Tell them the truth in a way that won’t hurt their feelings\"}, {\"answer\": \"Lie to make them feel better\"}, {\"answer\": \"Tell them the truth only if it won’t hurt their feelings\"}, {\"answer\": \"Tell them the truth regardless of whether it will hurt their feelings\"}],\"question11\": [],\"question12\": [3, \"Do you like following trends, or doing your own thing?\", {\"answer\": \"Following trends\"}, {\"answer\": \"Doing your own thing\"}, {\"answer\": \"It depends\"}],\"question13\": [3, \"Do you feel comfortable enough being your true self, or do you hide behind what you think others want to see?\", {\"answer\": \"Feel comfortable being yourself\"}, {\"answer\": \"Hide your true self\"}, {\"answer\": \"It depends\"}],\"question14\": [4, \"When leading a group, do you try to take into account everyone’s wants, or do you go with what you think is the best for the situation?\", {\"answer\": \"Take into account everyone’s wants\"}, {\"answer\": \"Do what is best for / ideal for the situation\"}, {\"answer\": \"I try to do both, but ultimately give into appeasing people’s wants\"}, {\"answer\": \"I try to do both, but ultimately lean more towards what is best for the situation\"}]\n}");
 const answerCount = data["questionCount"];
 
 let answers = [];
@@ -74,11 +74,21 @@ function submit() {
 }
 
 function changeAnswers(_answerArray) {
-    for(let i = 0; i < 4; i++) {
-        answerLabels[i].innerText = _answerArray[i];
+    for(let i = 2; i < _answerArray[0]+2; i++) {
+        answerLabels[i-2].innerText = _answerArray[i].answer;
     }
 
-    questionLabel.innerText = _answerArray[4];
+    for(let i = _answerArray[0]; i < 4; i++) {
+        answerLabels[i].style.display = "none";
+        answerButtons[i].style.display = "none";
+    }
+
+    for(let i = 0; i < _answerArray[0]; i++) {
+        answerLabels[i].style.display = "";
+        answerButtons[i].style.display = "";
+    }
+
+    questionLabel.innerText = _answerArray[1];
 }
 
 function checkAnswer() {
